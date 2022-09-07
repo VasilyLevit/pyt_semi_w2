ca5513a2bb4f16f55633080985a2f3f34f9d7f11
Vasily Levit <vasily.levit@gmail.com>
2022-09-07 20:51:35 +0800

    made part create list

--- homework3.py
+++ homework3.py
@@ -1,1 +1,10 @@
 # Задайте список из k чисел последовательности (1 + 1\k)^k и выведите на экран их сумму.
+
+k = int(input('Введите число k: '))
+result = 0
+summ = 0
+for i in range(1, k+1):
+    result = (1 + 1/i)**i
+    summ += result
+    print(result, end=' ')
+print(round(summ,2))
