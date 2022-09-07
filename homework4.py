d62477ca67355b0835c5c952cb12679d46137a37
Vasily Levit <vasily.levit@gmail.com>
2022-09-07 21:26:40 +0800

    add muliple arguments

--- homework4.py
+++ homework4.py
@@ -3,11 +3,12 @@
 
 n = 5
 
-string = input('Введите 2а числа через пробел: ')
-str_list = string.split(' ', 1)
+num_string = input('Введите 2а числа через пробел: ')
+list_num = num_string.split(' ', 1)
 
 for i in range(n):
     numbers = list(range(-n, n+1))
-
+arg_1 = numbers[int(list_num[0])]
+arg_2 = numbers[int(list_num[1])]
 print(numbers)
-print(str_list)
+print(arg_1*arg_2)
