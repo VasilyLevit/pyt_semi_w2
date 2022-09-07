55b223552acbf3e4fd7b164c7e64b9028033778e
Vasily Levit <vasily.levit@gmail.com>
2022-09-07 21:42:50 +0800

    Оформление

--- homework4.py
+++ homework4.py
@@ -1,14 +1,15 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных пользователем через пробел позициях. (потребуется сплит)
 
-n = 5
+n = 10
 
-num_string = input('Введите 2а числа через пробел: ')
+num_string = input('Введите 2а числа от 0 до 10 через пробел: ')
 list_num = num_string.split(' ', 1)
 
 for i in range(n):
     numbers = list(range(-n, n+1))
-arg_1 = numbers[int(list_num[0])]
-arg_2 = numbers[int(list_num[1])]
-print(numbers)
-print(arg_1*arg_2)
+elem_1 = numbers[int(list_num[0])]
+elem_2 = numbers[int(list_num[1])]
+mult = elem_1 * elem_2
+print('N =',numbers)
+print('N[{}] * N[{}] = {}'.format(list_num[0], list_num[1], mult))
