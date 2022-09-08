2eaf5a792a1d474f270404e839c160a2aedd4218
Vasily Levit <vasily.levit@gmail.com>
2022-09-08 21:28:19 +0800

    finish

--- homework4.py
+++ homework4.py
@@ -6,8 +6,7 @@ n = 10
 num_string = input('Введите 2а числа от 0 до 10 через пробел: ')
 list_num = num_string.split(' ', 1)
 
-for i in range(n):
-    numbers = list(range(-n, n+1))
+numbers = list(range(-n, n+1))
 elem_1 = numbers[int(list_num[0])]
 elem_2 = numbers[int(list_num[1])]
 mult = elem_1 * elem_2
--- homework5.py
+++ homework5.py
@@ -1,1 +1,17 @@
 # Реализуйте алгоритм перемешивания списка. (свой рандомайз)
+
+import random
+n = 10
+
+list_in = list(range(n))
+list_out = []
+length_list = len(list_in)
+
+print('List input =', list_in)
+
+for i in range(length_list):
+    num_rand = random.choice(list_in)
+    list_out.append(num_rand)
+    list_in.remove(num_rand)
+
+print('List output =', list_out)
