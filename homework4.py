b47bab116a238018f10d9e98bdfd890265babb17
Vasily Levit <vasily.levit@gmail.com>
2022-09-11 23:25:52 +0800

    добавлено перемножение любого числа позиций

--- homework4.py
+++ homework4.py
@@ -2,13 +2,14 @@
 # Найдите произведение элементов на указанных пользователем через пробел позициях. (потребуется сплит)
 
 n = 10
-
-num_string = input('Введите 2а числа от 0 до 10 через пробел: ')
-list_num = num_string.split(' ', 1)
-
 numbers = list(range(-n, n+1))
-elem_1 = numbers[int(list_num[0])]
-elem_2 = numbers[int(list_num[1])]
-mult = elem_1 * elem_2
 print('N =', numbers)
-print('N[{}] * N[{}] = {}'.format(list_num[0], list_num[1], mult))
+
+num_string = input('Введите числа от 0 до 10 через пробелы: ')
+positions = num_string.split(' ')
+
+mult = 1
+for i in positions:
+    mult *= numbers[int(i)]
+   
+print('Произведение: {}'.format(mult))
