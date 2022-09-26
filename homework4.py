6096443d0a68b08715f5448d938be95e16ba6b12
Vasily Levit <vasily.levit@gmail.com>
2022-09-26 17:13:12 +0800

    add var3 with argument-functions

--- homework4.py
+++ homework4.py
@@ -13,15 +13,33 @@
 # print('Произведение: {}'.format(result))
 
 # Вариант 2 - с использованием функций
-def mult_pos(numbers, pos):
+# def mult_pos(numbers, pos):
+#     result = 1
+#     for i in pos:
+#         result *= numbers[int(i)]
+#     return result
+# n = 10
+# numbers = list(range(-n, n+1))
+# print('N =', numbers)
+# num_string = input('Введите числа от 0 до 10 через пробелы: ')
+# positions = num_string.split(' ')
+
+# print('Произведение: {}'.format(mult_pos(numbers, positions)))
+
+# Вариант 3 - с использованием передачи функций в качестве аргумента
+def mult_pos(fun_gen, pos, n):
     result = 1
+    numbers = fun_gen(n)
     for i in pos:
         result *= numbers[int(i)]
     return result
-n = 10
-numbers = list(range(-n, n+1))
-print('N =', numbers)
-num_string = input('Введите числа от 0 до 10 через пробелы: ')
-positions = num_string.split(' ')
 
-print('Произведение: {}'.format(mult_pos(numbers, positions)))
+def list_gen(n):
+    numbers = list(range(-n, n+1))
+    return numbers
+
+n = 10
+print('N =', list_gen(n))
+positions = input('Введите числа от 0 до 10 через пробелы: ').split(' ')
+
+print('Произведение: {}'.format(mult_pos(list_gen, positions, n)))
