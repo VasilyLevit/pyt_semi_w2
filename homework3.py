daabedfceee1d4ce929ebe46c8fcf028e76f15d3
Vasily Levit <vasily.levit@gmail.com>
2022-09-27 12:17:56 +0800

    add comprehension in hometask2-3

--- homework3.py
+++ homework3.py
@@ -1,10 +1,14 @@
 # Задайте список из k чисел последовательности (1 + 1\k)^k и выведите на экран их сумму.
 
+# k = int(input('Введите число k: '))
+# result = 0
+# summ = 0
+# for i in range(1, k+1):
+#     result = (1 + 1/i)**i
+#     summ += result
+#     print(round(result,2), end=' ')
+# print('\n Cумма:', round(summ,2))
+
+# Вариант 2 с использованием List Comperhension
 k = int(input('Введите число k: '))
-result = 0
-summ = 0
-for i in range(1, k+1):
-    result = (1 + 1/i)**i
-    summ += result
-    print(round(result,2), end=' ')
-print('\n Cумма:', round(summ,2))
+print('\n Cумма:', sum((1 + 1/i)**i for i in range(1, k+1)))
